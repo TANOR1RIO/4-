71bb2cdf306218a53543ef7a7054487d8124e04d
Evgeniy <evgeniyvaxruhsev@gmail.com>
2024-12-17 19:06:50 +0500

    123
    123

---
+++ 4 практика/4 практика/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+/*class Programm
+{
+    static void Main()
+    {
+        int[] a = { 1, 2, 3, 4, 5, };
+        int[] b = { 6, 7, 8, 9, 10, };
+        int[] result = a.Concat(b).ToArray();
+        Console.WriteLine(string.Join(", ", result));
+    }
+    23123123123
+}*/
---
+++ 5 практика/5 практика/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+/*class Program
+{
+    static void Main()
+    {
+        int[] numbers = {8,4,2,5,6,6,2,4,5,6,7,8 };
+
+        var mostFrequentElement = numbers.GroupBy(x => x)
+                                         .OrderByDescending(g => g.Count())
+                                         .Select(g => g.Key)
+                                         .First();
+
+        Console.WriteLine($"Наиболее часто встречающийся элемент: {mostFrequentElement}");
+    }
+}*/
